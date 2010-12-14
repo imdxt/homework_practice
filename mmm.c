effeefa14f17cc4ec0f12deecf3b966ecf4f0742
dong <dong@ubuntu.(none)>
2010-12-14 08:27:37 +0800

    first

---
+++ mmm.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+    char a[]="abc";
+        a[1]=0;
+        printf("%s\n",a);
+        printf("%d\n",a[2]); 
+        printf("%d\n",a[6]);
+    return 0;
+}
