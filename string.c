cccb8a3beb73d4e48a557195947bc1f5b39ab7f0
dong <dong@ubuntu.(none)>
2010-12-14 09:27:41 +0800

    string first

---
+++ string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, const char *argv[])
+{
+   // int i;
+      char a,b,c;
+   // char a[5]="abcd";
+   // for(i=0;i<strlen(a);i++)
+   // {
+        a=getchar();
+        b=getchar();
+        c=getchar();
+        printf("%d\n",a);
+        printf("%d\n",b);
+        printf("%d\n",c);
+       // putchar(a[i]);
+       //putchar(c);
+   // }
+   //putchar('\n');
+    return 0;
+}
