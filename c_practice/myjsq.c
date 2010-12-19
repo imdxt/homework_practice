7e230657f6390bcfada320a1f103912a9bb21338
dong <dong@ubuntu.(none)>
2010-12-19 20:25:19 +0800

    放在一个文件里面

--- c_practice/myjsq.c
+++ c_practice/myjsq.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 typedef int (*fun)(int , int);
+//定义类型，为类型命名，与直接写出变量的类型定义变量效果完全相同。
+
 int add(int a, int b)  { return a+b;}
 int subtract(int a, int b) { return a-b;}
 int multiply(int a, int b) { return a*b;}
@@ -9,8 +11,8 @@ int main(int argc, const char *argv[])
 {
     int a,b,ad,su,mu,de,mo;
     char i;
-    fun add1;
-    add1=add;
+    fun add1;//申请一个地址空间
+    add1=add;//把函数add的入口地址赋给add1 
 
     fun subtract1;
     subtract1=subtract;
@@ -33,32 +35,74 @@ int main(int argc, const char *argv[])
           printf("Input num2:\n");
           scanf("%d",&b);
           ad=add1(a,b);
-          getchar();
+          getchar();//因为输入完成之后要回车才能输入字符，程序会把回车当字符读入造成程序出乱，所以加上getchar（）
     switch(i)
     {
         case '+':
           ad=add1(a,b);
-          printf("%d+%d=%d\n",a,b,ad);
+          printf("%d + %d= %d\n",a,b,ad);
           break;
          
         case '-':
          su=subtract1(a,b);
-          printf("%d-%d=%d\n",a,b,su);
+          printf("%d - %d= %d\n",a,b,su);
           break;
         case '*':
           mu=multiply1(a,b);
-          printf("%d*%d=%d\n",a,b,mu);
+          printf("%d * %d= %d\n",a,b,mu);
           break;
         case '/':
           de=dev1(a,b);
-          printf("%d/%d=%d\n",a,b,de);
+          printf("%d / %d= %d\n",a,b,de);
           break;
         case '%':
           mo=mod1(a,b);
-          printf("%d %% %d=%d\n",a,b,mo);
+          printf("%d %% %d= %d\n",a,b,mo);
+          //打印%的时候 中间要加一个%分开 否则会出错误
           break;
     }
 
     }
     return 0;
 }
+/******************************************************/
+// this is teacher write;
+#include <stdio.h>
+
+int add(int x, int y){return x + y;}
+int sub(int x, int y){return x - y;}
+int mul(int x, int y){return x * y;}
+int dev(int x, int y){return x / y;}
+int mod(int x, int y){return x % y;}
+struct cmd{
+    char op ;
+    int(* handle)(int, int);
+} ;//结构体
+void test(void){
+     double a;
+     scanf("%f", (float *)&a);
+}
+struct cmd arr[] = {
+    {'+',add},
+    {'-',sub},
+    {'*',mul},
+    {'/',dev},
+    {'%',mod}
+};
+int main(int argc, char * argv[]){
+    char op; int x,y;
+    int i, ret;
+    while(1)
+    {
+    scanf("%c", &op);
+
+    if(op == '#') break;
+    scanf("%d %d",&x, &y);
+    for(i = 0; i < 5; i++)                                                          {   
+    if(arr[i].op == op)
+        ret = arr[i].handle(x, y);
+    }                                                                               getchar();
+    printf("%d %c %d = %d\n", x, op, y, ret);
+    }
+    return 0;
+    }
