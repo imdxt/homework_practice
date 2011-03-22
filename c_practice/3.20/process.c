70a194a9c9ff6f00d09974a760bb6baa8ba6a8f0
dong <dong@ubuntu.(none)>
2011-03-22 18:15:02 +0800

    ADMN4

--- c_practice/3.20/process.c
+++ c_practice/3.20/process.c
@@ -8,29 +8,89 @@
 #include <strings.h>
 #include <string.h>
 #include <signal.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
-#define SERV_PORT 31000
+#define UDP_PORT 30000
+
+int get_filename(char *dir_p,char namebuf[][128])
+{
+    DIR *d;
+    struct dirent *file;
+    int len = 0;
+
+    if(!(d = opendir(dir_p)))
+    {
+        printf("Error opendir %s!!!\n",dir_p);
+        exit(1);
+    }
+    while((file = readdir(d)) != NULL)
+    {
+        if(strncmp(file->d_name,".",1) == 0)
+            continue;
+        strcpy(namebuf[len++],file->d_name);
+    }
+
+    closedir(d);
+    return 0;
+}
+
+void removefiles(char* dirname)
+{
+    DIR* d = opendir(dirname);
+    struct dirent* f;
+    char buf[256];
+
+    while((f = readdir(d)) != NULL)
+    {
+        if(*f->d_name == '.') continue;
+
+        if(f->d_type == DT_DIR) // sub dir
+        {
+            strcpy(buf, dirname);
+            strcpy(buf, "/");
+            strcpy(buf, f->d_name);
+            removefiles(buf);
+            
+        }
+        else if(f->d_type == DT_REG)
+        {
+            strcpy(buf, dirname);
+            strcat(buf, "/");
+            strcat(buf, f->d_name);
+            remove(buf);
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     pid_t pid;
     int s = socket(AF_INET, SOCK_DGRAM, 0);
-    //int client_sock;
     int sockfd,sendfd;
-    struct sockaddr_in addr,servaddr, sendaddr,clientaddr;
-    //socklen_t cliaddr_len;
-    char buf[1024] = "0";
-    char buf1[1024] = "0";
-    char temp[30] = "0";
-    char *p ,*e , *m;
+    struct sockaddr_in addr,servaddr, sendaddr;
+    char UDP_buf[1024] = "0";
+    char TCP_buf[1024] = "0";
+    char temp[128] = "0";
+    char PORT[30] = "0";
+    char *UDP_p ,*UDP_e;
+    int TCP_PORT;
     char *ID = argv[1];
-    int n;
+    char *type_p,*filename_p,*len_p,*TCP_e;
+    char namebuf[20][128];
+    int i;
     int flag = 8;
-    FILE *fp;
-  
+    int fd;
+
+    sprintf(PORT,"%s",ID+5);
+    TCP_PORT = 30001 + atoi(PORT);
+    printf("%d\n",TCP_PORT);
     bzero(&addr,sizeof(addr));
     bzero(&servaddr,sizeof(servaddr));
     bzero(&sendaddr,sizeof(sendaddr));
+
 exec_fork:
     pid = fork();
     switch(pid)
@@ -41,114 +101,128 @@ exec_fork:
 
         case 0:
             while(1)
-                execl("show","show",NULL);
+                execl("Adshow","Adshow",NULL);
 
         default:
-            //flag = 1;
             while(1)
             {
+                bzero(UDP_buf,sizeof(UDP_buf));
                 addr.sin_family = AF_INET;
-                addr.sin_port = htons(30000);
+                addr.sin_port = htons(UDP_PORT);
                 addr.sin_addr.s_addr = INADDR_ANY;
                 bind(s, (struct sockaddr*)&addr, sizeof(addr));
 
-                recv(s, buf, sizeof(buf), 0);
-                p = buf;
-                printf("%s\n",p);
-                if((p = strstr(p,ID)) == NULL)
+                recv(s, UDP_buf, sizeof(UDP_buf), 0);
+                UDP_p = UDP_buf;
+                printf("%s\n",UDP_p);
+                if((UDP_p = strstr(UDP_p,ID)) == NULL)
                 {
                     continue;
                 }
-                e = strstr(p,"&");
-                *e = 0;
-                p += strlen(ID);
-                switch(atoi(p))
+                UDP_e = strstr(UDP_p,"&");
+                *UDP_e = 0;
+                UDP_p += strlen(ID);
+                switch(atoi(UDP_p))
                 {
                     case 16://off
                         if(flag == 8)
                         {
                             kill(pid,SIGKILL);
-                            wait();
                             flag = 7;
+                            wait(NULL);
                         }
                         break;
                     case 13://on
                         if(flag == 7)
                         {
                             kill(pid,SIGKILL);
-                            wait();
                             flag = 8;
+                            wait(NULL);
                             goto exec_fork;
                         }
                         break;
                     case 15://发送当前状态
+                        bzero(temp,sizeof(temp));
                         sprintf(temp,"%s1%d",ID,flag);
                         printf("%s\n",temp);
                         sendfd = socket(AF_INET,SOCK_STREAM,0);
                         sendaddr.sin_family = AF_INET;
-                        sendaddr.sin_port = htons(SERV_PORT);
+                        sendaddr.sin_port = htons(31000);
                         sendaddr.sin_addr.s_addr = inet_addr("10.1.14.211");
 
                         if(connect(sendfd,(struct sockaddr *)&sendaddr,sizeof(sendaddr)) < 0)
                         {
                             printf("Connect server failed.\n");
-                            perror("COnnect");
                             exit(1);
                         }
                         write(sendfd,temp,sizeof(temp));
-#if 0
-                        if(bind(sendfd,(struct sockaddr *)&sendaddr,sizeof(sendaddr)) < 0)
-                        {
-                            printf("bind error\n");
-                            exit(1);
-                        }
-                        else printf("bind success!!\n");
-                        if(listen(sendfd,5) < 0)
-                        {
-                            printf("listen error\n");
-                            exit(1);
-                        }
-                        else printf("listen success!!\n");
-                        
-                        cliaddr_len = sizeof(clientaddr);
-                        if((client_sock = accept(sendfd,(struct sockaddr *)&clientaddr,&cliaddr_len)) < 0)
-                        {
-                            close(sendfd);
-                            printf("accept error\n");
-                            exit(1);
-                        }
-                        else write(client_sock,temp,sizeof(temp));
-#endif
-                        //close(client_sock);
                         close(sendfd);
                         printf("send temp success!!\n");
                         break;
                     case 19://文件同步
                         kill(pid,SIGKILL);
-                        wait();
+                        wait(NULL);
+                        bzero(temp,sizeof(temp));
+                        bzero(&namebuf,20*128*sizeof(char));
 
                         sockfd = socket(AF_INET,SOCK_STREAM,0);
                         servaddr.sin_family = AF_INET;
-                        servaddr.sin_port = htons(SERV_PORT);
-                        servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+                        servaddr.sin_port = htons(TCP_PORT);
+                        servaddr.sin_addr.s_addr = inet_addr("10.1.14.211");
 
                         if(connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) < 0)
                         {
                             printf("Connect server failed.\n");
                             exit(1);
                         }
-                        if((fp = fopen("/home/dong/imdxt","a+")) == NULL)
+                        read(sockfd,TCP_buf,128);
+                        
+                        printf("%s\n",TCP_buf);
+                        type_p = strstr(TCP_buf, "FiLeTyPe=");
+                        type_p += strlen("FiLeTyPe=");
+                        TCP_e = strstr(type_p,"&");
+                        *TCP_e = 0;
+                         
+                        filename_p = strstr(TCP_e+1,"FiLeNaMe=");
+                        filename_p += strlen("FiLeNaMe=");
+                        TCP_e = strstr(filename_p,"&");
+                        *TCP_e = 0;
+
+                        len_p = strstr(TCP_e+1,"FiLeLeNgHt=");
+                        len_p += strlen("FiLeLeNgHt=");
+
+                        sprintf(temp,"/home/dong/ADMN4/%s/",type_p);
+                        removefiles(temp);
+                        //get_filename(temp,namebuf);
+                        //for (i = 0; namebuf[i] != NULL; i++) 
+                        //{
+                        //printf("%s\n",namebuf[i]);
+                        //strcat(temp,namebuf[i]);
+                        //remove(temp);
+                        //}
+                        //printf("aaaaaaaaaaaaaaaaaaaaaaaa\n");
+                        bzero(temp,sizeof(temp));
+                       
+                        sprintf(temp,"/home/dong/ADMN4/%s/%s",type_p,filename_p);
+
+                        printf("%s\n",temp);
+                        if((fd = open(temp,O_WRONLY|O_CREAT,0666)) == -1)
                         {
-                            printf("fopen file error\n");
+                            printf("open file error\n");
                             exit(1);
                         }
 
-                        while((n = read(sockfd,buf1,1024)) > 0)
+                        for (i = 0; i < atoi(len_p)/1024; i++) 
                         {
-                            fwrite(buf1,n,1,fp);
+                            read(sockfd,TCP_buf,1024);
+                            write(fd,TCP_buf,1024);
                         }
+
+                        read(sockfd,TCP_buf,atoi(len_p)%1024);
+                        write(fd,TCP_buf,atoi(len_p)%1024);
+
                         printf("save file success!\n");
-                        fclose(fp);
+                        close(fd);
                         close(sockfd);
                         goto exec_fork;
                         break;
