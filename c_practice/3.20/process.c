5a14990097987c92e8ad10c899e88e3413db071a
dong <dong@ubuntu.(none)>
2011-03-23 18:17:45 +0800

    admn4_client

--- c_practice/3.20/process.c
+++ c_practice/3.20/process.c
@@ -13,9 +13,11 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-#define UDP_PORT 30000
+#define UDP_PORT 40000
+#define TCP_PORT1 31000
+#define TCP_PORT 40001
 
-int get_filename(char *dir_p,char namebuf[][128])
+int get_filename(char *dir_p,char namebuf[][128])//参数为 文件目录 存放文件名的缓冲buf
 {
     DIR *d;
     struct dirent *file;
@@ -55,14 +57,15 @@ void removefiles(char* dirname)
             removefiles(buf);
             
         }
-        else if(f->d_type == DT_REG)
+        else if(f->d_type == DT_REG)//如果是文件
         {
             strcpy(buf, dirname);
             strcat(buf, "/");
             strcat(buf, f->d_name);
-            remove(buf);
+          remove(buf);
         }
     }
+    closedir(d);
 }
 
 int main(int argc, char *argv[])
@@ -76,7 +79,6 @@ int main(int argc, char *argv[])
     char temp[128] = "0";
     char PORT[30] = "0";
     char *UDP_p ,*UDP_e;
-    int TCP_PORT;
     char *ID = argv[1];
     char *type_p,*filename_p,*len_p,*TCP_e;
     char namebuf[20][128];
@@ -85,8 +87,8 @@ int main(int argc, char *argv[])
     int fd;
 
     sprintf(PORT,"%s",ID+5);
-    TCP_PORT = 30001 + atoi(PORT);
-    printf("%d\n",TCP_PORT);
+    //TCP_PORT = 20001 + atoi(PORT);
+    //printf("%d\n",TCP_PORT);
     bzero(&addr,sizeof(addr));
     bzero(&servaddr,sizeof(servaddr));
     bzero(&sendaddr,sizeof(sendaddr));
@@ -114,13 +116,13 @@ exec_fork:
 
                 recv(s, UDP_buf, sizeof(UDP_buf), 0);
                 UDP_p = UDP_buf;
-                printf("%s\n",UDP_p);
                 if((UDP_p = strstr(UDP_p,ID)) == NULL)
                 {
                     continue;
                 }
                 UDP_e = strstr(UDP_p,"&");
                 *UDP_e = 0;
+                printf("%s\n",UDP_p);
                 UDP_p += strlen(ID);
                 switch(atoi(UDP_p))
                 {
@@ -147,7 +149,7 @@ exec_fork:
                         printf("%s\n",temp);
                         sendfd = socket(AF_INET,SOCK_STREAM,0);
                         sendaddr.sin_family = AF_INET;
-                        sendaddr.sin_port = htons(31000);
+                        sendaddr.sin_port = htons(TCP_PORT1);
                         sendaddr.sin_addr.s_addr = inet_addr("10.1.14.211");
 
                         if(connect(sendfd,(struct sockaddr *)&sendaddr,sizeof(sendaddr)) < 0)
@@ -163,11 +165,12 @@ exec_fork:
                         kill(pid,SIGKILL);
                         wait(NULL);
                         bzero(temp,sizeof(temp));
+                        
                         bzero(&namebuf,20*128*sizeof(char));
 
                         sockfd = socket(AF_INET,SOCK_STREAM,0);
                         servaddr.sin_family = AF_INET;
-                        servaddr.sin_port = htons(TCP_PORT);
+                        servaddr.sin_port = htons(TCP_PORT+atoi(PORT));
                         servaddr.sin_addr.s_addr = inet_addr("10.1.14.211");
 
                         if(connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) < 0)
@@ -175,53 +178,80 @@ exec_fork:
                             printf("Connect server failed.\n");
                             exit(1);
                         }
-                        read(sockfd,TCP_buf,128);
-                        
-                        printf("%s\n",TCP_buf);
-                        type_p = strstr(TCP_buf, "FiLeTyPe=");
-                        type_p += strlen("FiLeTyPe=");
-                        TCP_e = strstr(type_p,"&");
-                        *TCP_e = 0;
-                         
-                        filename_p = strstr(TCP_e+1,"FiLeNaMe=");
-                        filename_p += strlen("FiLeNaMe=");
-                        TCP_e = strstr(filename_p,"&");
-                        *TCP_e = 0;
-
-                        len_p = strstr(TCP_e+1,"FiLeLeNgHt=");
-                        len_p += strlen("FiLeLeNgHt=");
-
-                        sprintf(temp,"/home/dong/ADMN4/%s/",type_p);
-                        removefiles(temp);
-                        //get_filename(temp,namebuf);
-                        //for (i = 0; namebuf[i] != NULL; i++) 
+                        removefiles("/home/dong/ADMN4/vedio/");
+                        removefiles("/home/dong/ADMN4/image/");
+                        removefiles("/home/dong/ADMN4/text/");
+                        removefiles("/home/dong/ADMN4/other/");
+                        printf("等待接收-----!!!!\n");
+                        int c_len = 0;
+                        int size = 4;
+                        read(sockfd,&c_len,size);
+                        printf("%d\n",c_len);
+                        //while(1)
                         //{
-                        //printf("%s\n",namebuf[i]);
-                        //strcat(temp,namebuf[i]);
-                        //remove(temp);
-                        //}
-                        //printf("aaaaaaaaaaaaaaaaaaaaaaaa\n");
-                        bzero(temp,sizeof(temp));
-                       
-                        sprintf(temp,"/home/dong/ADMN4/%s/%s",type_p,filename_p);
+                        #if 1
+                            
+                            int ret = 0;
 
-                        printf("%s\n",temp);
-                        if((fd = open(temp,O_WRONLY|O_CREAT,0666)) == -1)
-                        {
-                            printf("open file error\n");
-                            exit(1);
-                        }
+                            while(1)
+                            {
+                                ret += read(sockfd, TCP_buf+ret, c_len-ret);
+                                if(ret == c_len) break;
+                            }
 
-                        for (i = 0; i < atoi(len_p)/1024; i++) 
-                        {
-                            read(sockfd,TCP_buf,1024);
-                            write(fd,TCP_buf,1024);
-                        }
+                            printf("%s\n",TCP_buf);
 
-                        read(sockfd,TCP_buf,atoi(len_p)%1024);
-                        write(fd,TCP_buf,atoi(len_p)%1024);
+                            type_p = strstr(TCP_buf, "FiLeTyPe=");
+                            type_p += strlen("FiLeTyPe=");
+                            TCP_e = strstr(type_p,"&");
+                            *TCP_e = 0;
+                             
+                            filename_p = strstr(TCP_e+1,"FiLeNaMe=");
+                            filename_p += strlen("FiLeNaMe=");
+                            TCP_e = strstr(filename_p,"&");
+                            *TCP_e = 0;
 
-                        printf("save file success!\n");
+                            len_p = strstr(TCP_e+1,"FiLeLeNgHt=");
+                            len_p += strlen("FiLeLeNgHt=");
+                            c_len =  atoi(len_p);
+
+                           
+                            sprintf(temp,"/home/dong/ADMN4/%s/%s",type_p,filename_p);
+
+                            printf("%s\n",temp);
+                            if((fd = open(temp,O_WRONLY|O_CREAT,0666)) == -1)
+                            {
+                                printf("open file error\n");
+                                exit(1);
+                            }
+
+                            int bufsize = 1024;
+                            for (i = 0; i < c_len/1024; i++) 
+                            {
+                                ret = 0;
+                                while(1)
+                                {
+                                    ret += read(sockfd, TCP_buf+ret, bufsize-ret);
+                                    if(ret == bufsize) break;
+                                }
+                                //read(sockfd,TCP_buf,1024);
+                                perror("read11");
+                                write(fd,TCP_buf,bufsize);
+                            }
+                            bzero(TCP_buf,sizeof(TCP_buf));
+                            ret = 0;
+                            c_len = c_len%1024;
+                            while(1)
+                            {
+                                ret += read(sockfd, TCP_buf+ret, c_len-ret);
+                                if(ret == c_len) break;
+                            }
+                            printf("len ----%s\n",TCP_buf);
+                            write(fd,TCP_buf,c_len);
+#endif
+                            printf("save file success!\n");
+                            //}
+                            while(1);
                         close(fd);
                         close(sockfd);
                         goto exec_fork;
